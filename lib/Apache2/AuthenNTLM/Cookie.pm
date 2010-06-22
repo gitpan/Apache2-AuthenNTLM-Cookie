8a53f73520fc9bc93934aa1fb0ca097bec310cbf
Laurent Dami <dami@cpan.org>
2010-06-22 21:13:30 +0000

    Import of DAMI/Apache2-AuthenNTLM-Cookie-1.00 from CPAN.
    gitpan-cpan-distribution: Apache2-AuthenNTLM-Cookie
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         DAMI/Apache2-AuthenNTLM-Cookie-1.00.tar.gz
    gitpan-cpan-author:       DAMI
    gitpan-cpan-maturity:     released

--- lib/Apache2/AuthenNTLM/Cookie.pm
+++ lib/Apache2/AuthenNTLM/Cookie.pm
@@ -1,43 +1,76 @@
+# see doc at end of file
+
 package Apache2::AuthenNTLM::Cookie;
 
 use strict;
 use warnings;
 
-use Apache2::RequestRec ();
+use Apache2::RequestRec        ();
+use Apache2::Request;
 use Apache2::Cookie;
-use Apache2::Directive ();
-use Apache2::Const -compile => qw(OK) ;
-
-use Digest::SHA1  qw(sha1_hex);
+use Apache2::Directive         ();
+use Apache2::Const -compile => qw(OK HTTP_UNAUTHORIZED) ;
+use Digest::SHA1               qw(sha1_hex);
+use MIME::Base64               ();
 
 use base 'Apache2::AuthenNTLM';
 
-our $VERSION = '0.01';
+our $VERSION = '1.00';
 
-my $PACK_STRING = "A40 A12 A*"; # digest(40); time_created(12); username
+# constants from NTLM protocol
+use constant NEGOTIATE_UNICODE     => 0x00000001;
+use constant NEGOTIATE_NTLM        => 0x00000200;
+use constant TARGET_TYPE_DOMAIN    => 0x00010000;
+use constant NEGOTIATE_TARGET_INFO => 0x00800000;
+use constant NTLM_SIGNATURE        => "NTLMSSP";
+use constant NTLM_FORMAT           => "a8 V a8 V a8 a8 a8";
+
+# cookie format: digest(40); time_created(12); username
+use constant COOKIE_FORMAT         => "A40 A12 A*"; 
+
 
 sub handler : method  {
   my ($class, $r) = @_ ;
 
+  # create an instance
   my $self = bless {
     request     => $r,
     secret      => $r->dir_config('secret')      || $class->default_secret,
-    refresh     => $r->dir_config('refresh')     || 3600, # in seconds
+    refresh     => $r->dir_config('refresh')     || 14400, # in seconds
     cookie_name => $r->dir_config('cookie_name') || 'NTLM_AUTHEN',
    }, $class;
+
+  my $result;
 
   # get the cookie
   my $jar    = Apache2::Cookie::Jar->new($r);
   my $cookie = $jar->cookies($self->{cookie_name});
+  my $has_valid_cookie = $cookie && $self->validate_cookie($cookie->value);
 
-  # OK if cookie present and valid
-  return Apache2::Const::OK 
-    if $cookie and $self->validate_cookie($cookie->value);
+  # if cookie is present and valid
+  if ($has_valid_cookie) {
 
-  # otherwise, go through the NTLM handshake and then create the cookie
-  $r->log->debug("AuthenNTLM::Cookie: calling SUPER (NTLM handler)");
-  my $result = $class->SUPER::handler($r); # if success, will set $r->user
-  $self->set_cookie if $result == Apache2::Const::OK;
+    # if MSIE "optimization" is activated, i.e. if this is a POST with an NTLM
+    # type1 message and without body, then we must fake a Auth denial with an
+    # NTLM type2 message
+    my $must_fake_NTLM_msg2 = $r->method eq 'POST' && $self->has_empty_body
+                                                   && $self->is_NTLM_msg1;
+
+    # either respond with fake NTLM msg2, or authentication succeeded
+    $result = $must_fake_NTLM_msg2 ? $self->deny_with_NTLM_msg2
+                                   : Apache2::Const::OK;
+  }
+
+  # otherwise (if cookie is absent or invalid)
+  else {
+    # invoke parent class to go through the NTLM handshake
+    my $msg = $cookie ? "cookie invalidated" : "no cookie";
+    $r->log->debug("AuthenNTLM::Cookie: $msg, calling SUPER (NTLM handler)");
+    $result = $class->SUPER::handler($r); # if success, will set $r->user
+
+    # create the cookie if NTLM succeeded
+    $self->set_cookie if $result == Apache2::Const::OK;
+  }
 
   return $result;
 }
@@ -47,7 +80,7 @@ sub validate_cookie : method {
   my ($self, $cookie_val) = @_;
 
   # unpack cookie information
-  my ($sha, $time_created, $username) = unpack $PACK_STRING, $cookie_val;
+  my ($sha, $time_created, $username) = unpack COOKIE_FORMAT, $cookie_val;
 
   # valid if not too old and matches the SHA1 digest
   my $now = time;
@@ -72,7 +105,7 @@ sub set_cookie : method {
   my $username    = $r->user; # was just set from the parent handler
   my $now         = time;
   my $sha         = sha1_hex($now, $username, $self->{secret});
-  my $cookie_val  = pack $PACK_STRING, $sha, $now, $username;
+  my $cookie_val  = pack COOKIE_FORMAT, $sha, $now, $username;
   my @cookie_args = (-name => $self->{cookie_name}, -value => $cookie_val);
 
   # other cookie args may come from apache config
@@ -95,17 +128,63 @@ sub default_secret {
 
   # default secret : mtime and i-node of Apache configuration file
   my $config_file     = Apache2::Directive::conftree->filename;
-  my ($mtime, $inode) = (stat $config_file)[9, 1];  
+  my ($mtime, $inode) = (stat $config_file)[9, 1];
   return $mtime . $inode;
 }
 
 
-1;
+sub has_empty_body {
+  my $self = shift;
+  my $apr_req        = Apache2::Request->new($self->{request});
+  my $body_apr_table = $apr_req->body;
+  return !$body_apr_table || !scalar(keys %$body_apr_table);
+}
+
+
+sub is_NTLM_msg1 {
+  my $self = shift;
+  my $auth = $self->{request}->headers_in->{Authorization};
+  my $is_msg1 = 0;
+  if ($auth && $auth =~ s/^NTLM//) {
+    my $ntlm_msg = MIME::Base64::decode($auth);
+    my ($signature, $msg_type, $target_name,
+        $flags, $challenge, $context, $target_info) 
+      = unpack NTLM_FORMAT, $ntlm_msg;
+    $is_msg1 = $msg_type == 1;
+  }
+
+  return $is_msg1;
+}
+
+
+sub deny_with_NTLM_msg2 {
+  my $self = shift;
+
+  my $msg_type    = 2;
+  my $target_name = "";
+  my $flags       = NEGOTIATE_UNICODE  | NEGOTIATE_NTLM |
+                    TARGET_TYPE_DOMAIN | NEGOTIATE_TARGET_INFO;
+  my $challenge   = "";
+  my $context     = "";
+  my $target_info = "";
+
+  my $msg    = pack NTLM_FORMAT, NTLM_SIGNATURE, $msg_type, $target_name,
+                                 $flags, $challenge, $context, $target_info;
+  my $header = "NTLM " . MIME::Base64::encode($msg, '');
+
+  my $r   = $self->{request};
+  my $hdr = $r->err_headers_out;
+  $hdr->add($r->proxyreq ? 'Proxy-Authenticate' : 'WWW-Authenticate', $header);
+  $r->log->debug("AuthenNTLM::Cookie: creating fake type2 msg [$header]");
+
+  return Apache2::Const::HTTP_UNAUTHORIZED;
+}
+
+
+1; # End of Apache2::AuthenNTLM::Cookie
 
 
 __END__
-
-
 
 =head1 NAME
 
@@ -143,6 +222,8 @@ A similar module was already published on CPAN for Apache1 / modperl1
 for Apache2 / modperl2, and has a a different algorithm for cookie
 generation, in order to prevent any attempt to forge a fake cookie.
 
+Details about the NTLM authentication protocol can be found at
+L<http://davenport.sourceforge.net/ntlm.html#ntlmHttpAuthentication>.
 
 =head1 CONFIGURATION
 
@@ -167,7 +248,7 @@ allow you to control various details of cookie generation :
    PerlSetVar domain      my_cookie_domain  # default is none
    PerlSetVar expires     my_cookie_expires # default is none
    PerlSetVar path        my_cookie_path    # default is none
-   PerlSetVar refresh     some_seconds      # default is 3600 (1 hour)
+   PerlSetVar refresh     some_seconds      # default is 14400 (4 hours)
    PerlSetVar secret      my_secret_string  # default from stat(config file)
 
 See L<Apache2::Cookie> for explanation of variables
@@ -186,8 +267,7 @@ value is 0, which means that this is a session cookie, staying as long
 as the browser is open. But if the Windows account gets disabled,
 the cookie will never reflect the new situation : therefore we 
 must impose a periodic refresh of the cookie. The default refresh 
-value is 3600 seconds (one hour).
-
+value is 14400 seconds (four hours).
 
 =item secret
 
@@ -195,7 +275,7 @@ This is a secret phrase for generating a SHA1 digest that will be
 incorporated into the cookie. The digest also incorporates the
 username and cookie creation time, and is checked at each request :
 therefore it is impossible to forge a fake cookie without knowing the
-secret. 
+secret.
 
 The default value for the secret is the concatenation of modification
 time and inode of the F<httpd.conf> file on the server; therefore if
@@ -204,6 +284,25 @@ automatically invalidated.
 
 =back
 
+=head1 SPECIAL NOTE ABOUT INTERNET EXPLORER
+
+Microsoft Internet Explorer (MSIE) has an "optimization" when sending
+POST requests to an NTLM-secured site : the browser does not send the
+request body because it expects to receive a 401 HTTP_UNAUTHORIZED
+response, and then would send the body only at the second
+request. This is a problem with the present module, because if
+authorization is granted on the basis of a cookie, instead of NTLM
+handshake, then control goes to the HTTP response handler ... but that
+handler gets no parameters since the request body is empty !
+
+One way to fix the problem is to set the registry entry 
+C<HKEY_CURRENT_USER/Software/Microsoft/Windows/CurrentVersion/InternetSettings/DisableNTLMPreAuth>;
+but this is only feasible if one has control over the registry settings of all clients.
+Otherwise, the present module will forge a fake "NTLM type2" message, so that
+Internet will try again, sending a new NTLM type3 request with a proper body.
+See 
+L<http://lists.samba.org/archive/jcifs/2006-September/006554.html>
+for more details about this issue.
 
 =head1 AUTHOR
 
@@ -257,11 +356,11 @@ has been successfully tested on Apache2.2/modperl2/solaris.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Laurent Dami, all rights reserved.
+Copyright 2008,2010 Laurent Dami, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
 
-1; # End of Apache2::AuthenNTLM::Cookie
+
